f646065dc4208871fd9f053d7470f54cff3a7ce9
nick909 <princessblair.cm@gmail.com>
2025-06-04 21:41:26 +0200

    complete change of the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
 
-import icon from "astro-icon";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [icon()]
+    plugins: [tailwindcss()]
+  }
 });
